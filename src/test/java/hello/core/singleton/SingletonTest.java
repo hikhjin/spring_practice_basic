dba8bd524390b40ec0752c3164d681c52bf493e6
hikhjin <hikhjin@gmail.com>
2024-02-14 14:33:44 +0900

    singleton

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -28,7 +28,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceest() {
+    void singletonServiceTest() {
         SingletonService singletonService1 = SingletonService.getInstance();
         SingletonService singletonService2 = SingletonService.getInstance();
 

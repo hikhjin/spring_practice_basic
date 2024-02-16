c48f00f33712579b70358ac24b6171faee23030c
hikhjin <hikhjin@gmail.com>
2024-02-16 23:04:01 +0900

    Component

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+) // @Configuration 붙은 것들을 빈 등록에서 제외
+public class AutoAppConfig {
+}

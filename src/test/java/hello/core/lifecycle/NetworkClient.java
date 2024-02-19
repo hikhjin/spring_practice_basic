0eb7b3f277946133e93fc7a2cf23d404cea97a72
hikhjin <hikhjin@gmail.com>
2024-02-19 17:33:52 +0900

    bean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,11 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
     private String url;
 
     public NetworkClient() {
@@ -28,13 +30,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void destroy() throws Exception {
+
+    @PostConstruct
+    public void close() {
         disconnect();
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    @PreDestroy
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
